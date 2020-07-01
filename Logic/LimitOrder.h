ded1a2a4bb7ef2549362fc32cf9fd56112e0a8f7
Expurple <56318093+Expurple@users.noreply.github.com>
2020-07-01 20:13:44 +0300

    создал LimitOrder.h и папку Logic
    В Logic будет лежать то, что относится к обработке данных в экономическом или прикладном смысле. Применительно к стакану: это наверное всё кроме парсера и UI.

---
+++ Logic/LimitOrder.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+// Простая структура для хранения информации о лимитном ордере,
+// достаточной чтобы нарисовать график глубины рынка ("стакан").
+
+struct LimitOrder
+{
+	enum DealType {
+		BUY = 0,
+		SELL = 1
+	};
+
+	int instrumentId;
+	int priceInCents;
+	int quantity;
+	DealType dealType;
+};
