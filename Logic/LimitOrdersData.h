cb047bc5e2c8c60ee96fa4f368345da74ea9d635
Expurple <56318093+Expurple@users.noreply.github.com>
2020-07-02 16:42:44 +0300

    в основном сделал LimitOrdersData, но Debug ругается
    Изменял мэйн, чтобы протестить. В Release конфигурации всё ок запускается, но Debug выдаёт "debug assertion failed" из-за того как я в .cpp'шнике по-бандитски привожу итератор к обычному указателю через &* (в строчках 38, 42, 47). Скоро поменяю, чтоб не ругался.

---
+++ Logic/LimitOrdersData.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <map>
+#include <set>
+#include <vector>
+#include <utility>
+
+#include "LimitOrder.h"
+
+// Выдаёт полученные парсером данные отдельно по инструментам.
+
+class LimitOrdersData
+{
+public:
+
+	LimitOrdersData(std::vector<LimitOrder> *dataFromParser);
+	std::set<long long>& getInstrumentIdList();
+	std::pair<LimitOrder*, LimitOrder*> getOrdersRangeForInstrument(long long instrumentId);
+
+private:
+
+	std::vector<LimitOrder> * data;
+	std::set<long long> instrumentIdList;
+	std::map<long long, std::pair<LimitOrder*, LimitOrder*>> instrumentRanges;
+
+	void saveInstrumentRanges();
+	void sortInstrumentRangesByPrice();
+};
