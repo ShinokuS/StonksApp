9ad3a6a67a66c7a4003f3f40dcc0b0cacd01542e
Expurple <56318093+Expurple@users.noreply.github.com>
2020-07-03 15:04:37 +0300

    создал OrderBook

---
+++ Logic/OrderBook.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <map>
+#include <string>
+
+// Хранит всю информацию об ордерах, нужную для отрисовки стакана
+
+class OrderBook
+{
+public:
+
+	std::string instrument;
+
+	std::map<long long, int> asksAmountForPrice;
+	std::map<long long, int> bidsAmountForPrice;
+	
+	long long minPrice;
+	long long midPrice;
+	long long maxPrice;
+};
+
