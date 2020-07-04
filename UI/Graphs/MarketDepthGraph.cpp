340cffd64d3a6fb3a7946e4e41a6a619e316e913
Expurple <56318093+Expurple@users.noreply.github.com>
2020-07-05 00:58:59 +0300

    починил сломавшиеся оси, плюс ещё чуть-чуть реструктуризации

--- UI/Graphs/MarketDepthGraph.cpp
+++ UI/Graphs/MarketDepthGraph.cpp
@@ -8,25 +8,23 @@
 MarketDepthGraph::MarketDepthGraph(QAreaSeries* bidsSeries, QAreaSeries* asksSeries)
 {
     this->setTitle("Market Depth");
-    this->createDefaultAxes();
 
-    // будущая фича для расширения полей у оси Y
-    //this->axes(Qt::Horizontal).first()->setRange(0, 20);
-    //this->axes(Qt::Vertical).first()->setRange(0, 10);
-
+    // Нанесение самих линий графика:
     this->addSeries(bidsSeries);
-    bidsSeries->setName("Bids");
     QPen pen(0x059605);
     pen.setWidth(1);
     bidsSeries->setPen(pen);
 
     this->addSeries(asksSeries);
-    asksSeries->setName("Asks");
     pen.setColor(0x940000);
     pen.setWidth(1);
     asksSeries->setPen(pen);
 
-    // Красивое заполнение прощади под линиями
+    // И подписей к ним.
+    bidsSeries->setName("Bids");
+    asksSeries->setName("Asks");
+
+    // Красивое заполнение прощади под линиями:
     QLinearGradient gradient(QPointF(0, 0), QPointF(0, 1));
     gradient.setColorAt(0.0, 0x26f626);
     gradient.setColorAt(1.0, 0x1e671e);
@@ -35,4 +33,11 @@ MarketDepthGraph::MarketDepthGraph(QAreaSeries* bidsSeries, QAreaSeries* asksSer
     gradient.setColorAt(0.0, 0xe60000);
     gradient.setColorAt(1.0, 0x5c0006);
     asksSeries->setBrush(gradient);
+
+    // Оси:
+    this->createDefaultAxes();
+
+    // будущая фича для расширения полей у оси Y
+    //this->axes(Qt::Horizontal).first()->setRange(0, 20);
+    //this->axes(Qt::Vertical).first()->setRange(0, 10);
 }
