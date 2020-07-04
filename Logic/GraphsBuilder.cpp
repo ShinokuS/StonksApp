a6da0887717a4c73bba574b71f326d88a691224c
ShinokuS <56312449+ShinokuS@users.noreply.github.com>
2020-07-04 20:33:29 +0300

    изменены цвета на графике

--- Logic/GraphsBuilder.cpp
+++ Logic/GraphsBuilder.cpp
@@ -62,12 +62,12 @@ MarketDepthGraph* GraphsBuilder::buildMarketDepthGraph(OrderBook* orderBook) {
 
 
     QLinearGradient gradient(QPointF(0, 0), QPointF(0, 1));
-    gradient.setColorAt(0.0, 0x3cc63c);
-    gradient.setColorAt(1.0, 0x26f626);
+    gradient.setColorAt(0.0, 0x26f626);
+    gradient.setColorAt(1.0, 0x1e671e);
     gradient.setCoordinateMode(QGradient::ObjectBoundingMode);
     bidsSeries->setBrush(gradient);
-    gradient.setColorAt(0.0, 0xcc0000);
-    gradient.setColorAt(1.0, 0xe60000);
+    gradient.setColorAt(0.0, 0xe60000);
+    gradient.setColorAt(1.0, 0x5c0006);
     asksSeries->setBrush(gradient);
 
     //создаем макет графика
