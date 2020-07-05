2a04b5683949e6dbc6dbbb71938521c555c2289a
ShinokuS <56312449+ShinokuS@users.noreply.github.com>
2020-07-05 15:42:43 +0300

    рефакторинг кода для отрисовки окна
    перенесены все подключаемые файлы и библиотеки в StonksMainWindow.h; добавлен метод в класс окна для отрисовки графика в центральном виджете

---
+++ UI/StonksMainWindow.cpp
@@ -0,0 +1,22 @@
+﻿#include "StonksMainWindow.h"
+
+#include <QtCharts/QChartView>
+
+using namespace QtCharts;
+
+StonksMainWindow::StonksMainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+void StonksMainWindow::placeMarketDepthGraph()
+{
+    auto orderBook = OrderBook::getTestOrderBook();
+    auto graph = GraphsBuilder::buildMarketDepthGraph(orderBook);
+
+    QChartView* MarketDepthView = new QChartView(graph);
+    MarketDepthView->setRenderHint(QPainter::Antialiasing);
+
+    this->setCentralWidget(MarketDepthView);
+}
---
+++ UI/StonksMainWindow.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <QtWidgets/QMainWindow>
+
+
+#include "ui_StonksApp.h"
+#include "../Logic/OrderBook.h"
+#include "../Logic/GraphsBuilder.h"
+#include "Graphs/MarketDepthGraph.h"
+
+class StonksMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    StonksMainWindow(QWidget* parent = Q_NULLPTR);
+
+    void placeMarketDepthGraph();
+
+private:
+    Ui::StonksAppClass ui;
+
+};
