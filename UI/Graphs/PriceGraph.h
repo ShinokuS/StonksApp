ec15ecd76a1ed0f46a553121dc1806b64e747944
ShinokuS <56312449+ShinokuS@users.noreply.github.com>
2020-07-12 18:56:14 +0300

    добавил класс визуального отображения графика с методом отрисовки линейного графика, возможно в будущем потребуется другой формат отображения графика

---
+++ UI/Graphs/PriceGraph.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "QtCharts/qchart.h"
+#include <QtCharts/QLineSeries>
+
+using QtCharts::QChart;
+using QtCharts::QLineSeries;
+
+class PriceGraph
+{
+	void LinePriceGraph(QLineSeries* lineSeries);
+};
