17cdb4cc064ca2e5e5fab5290802adcfdb947685
ShinokuS <56312449+ShinokuS@users.noreply.github.com>
2020-07-12 18:42:40 +0300

    добавил модель хранения сделок

---
+++ Logic/Deals.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Order.h"
+
+#include <QtGlobal>
+
+class Deals
+{
+public:
+	void addNewDeal(qreal price, time_t time);
+	void getLastDeal(qreal price, time_t time);
+
+private:
+	Order* lastDeal;
+};
