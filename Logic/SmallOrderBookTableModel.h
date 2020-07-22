c1e18fe0b251a0871addfb504fd54ccdc7ceed7b
Дмитрий Александров <adk230@yandex.ru>
2020-07-22 21:10:27 +0300

    затабил пробелы

--- Logic/SmallOrderBookTableModel.h
+++ Logic/SmallOrderBookTableModel.h
@@ -9,21 +9,21 @@ class SmallOrderBookTableModel : public QAbstractTableModel
 {
 	Q_OBJECT
 public:
-    QList<Order*> rows;
-    SmallOrderBookTableModel(QObject* parent = nullptr);
+	QList<Order*> rows;
+	SmallOrderBookTableModel(QObject* parent = nullptr);
 
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
-    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-    bool setData(const QModelIndex& index, const QVariant& value, int role) override;
+	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+	int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+	Qt::ItemFlags flags(const QModelIndex& index) const override;
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+	bool setData(const QModelIndex& index, const QVariant& value, int role) override;
 
-    // Изменение данных таблицы
-    void changeData(OrderBookTableModel* orderBook);
-    void updateTable();
+	// Изменение данных таблицы
+	void changeData(OrderBookTableModel* orderBook);
+	void updateTable();
 
 private:
-    QStringList headers;
+	QStringList headers;
 
 };
