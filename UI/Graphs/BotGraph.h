a8f2585ca18762aac5204efd3db29cc6c9923f4a
ShinokuS <56312449+ShinokuS@users.noreply.github.com>
2020-07-23 11:53:34 +0300

    настроил маркеры бота

--- UI/Graphs/BotGraph.h
+++ UI/Graphs/BotGraph.h
@@ -1,1 +1,12 @@
-#pragma once
+﻿#pragma once
+
+#include "../../Logic/QCustomPlot/qcustomplot.h"
+#include "PriceGraph.h"
+
+#include <QDateTime>
+
+class BotGraph : public QCustomPlot
+{
+public:
+	BotGraph(QVector <double> time, QVector <double> price, PriceGraph* priceGraph);
+};
