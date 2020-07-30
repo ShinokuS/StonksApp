6cca8cf7276ece93953b931480a42f237f2070ac
Дмитрий Александров <adk230@yandex.ru>
2020-07-30 15:54:30 +0300

    убрал кишки бота в private

--- Logic/BotLogic.h
+++ Logic/BotLogic.h
@@ -5,9 +5,23 @@
 class BotLogic
 {
 public:
+
+	void reactAtNewDeal(Order* newDeal);
+
+	double botBalance = 1000;
+	int botThingsQuantity = 0;
+
+	QVector <double> timeBuy;
+	QVector <double> timeSell;
+
+	QVector <double> priceBuy;
+	QVector <double> priceSell;
+
+private:
+
 	int CHEKING_TIME_INTERVAL = 30;
 	double PURCHASE_PERCENTAGE = 0.01;
-	double SALE_PERCENTAGE = 0.1; 
+	double SALE_PERCENTAGE = 0.1;
 
 	int firtsIntervalTime;
 	double currentAverPrice;
@@ -19,18 +33,7 @@ public:
 
 	double sumOfPricesOnInterval;
 	int countOfDealsOnInterval;
-	int countOfIntervals=1;
+	int countOfIntervals = 1;
 
 	double lastPurchasePrice;
-
-	double botBalance = 1000;
-	int botThingsQuantity = 0;
-
-	QVector <double> timeBuy;
-	QVector <double> timeSell;
-
-	QVector <double> priceBuy;
-	QVector <double> priceSell;
-
-	void reactAtNewDeal(Order* newDeal);
 };
