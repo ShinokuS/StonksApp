66ca986924aef73bdd9824afe2da83fcf0650b18
Дмитрий Александров <adk230@yandex.ru>
2020-07-30 15:48:50 +0300

    выпилил из бота закомменченную старую страту

--- Logic/BotLogic.cpp
+++ Logic/BotLogic.cpp
@@ -2,16 +2,6 @@
 
 void BotLogic::reactAtNewDeal(Order* deal)
 {
-	/*
-	if (deal->price < BORDER_BUY_PRICE) {
-		priceBuy.append(deal->price);
-		timeBuy.append(deal->time);
-	}
-	if (deal->price > BORDER_SELL_PRICE) {
-		priceSell.append(deal->price);
-		timeSell.append(deal->time);
-	}
-	*/
 	sumOfPrices += deal->price;
 	countOfDeals++;
 	AverPrice = sumOfPrices / countOfDeals;
